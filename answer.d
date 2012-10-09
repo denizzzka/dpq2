244f85d8a4894938de208f62a812ec98b95e88e0
denizzzka <4denizzz@gmail.com>
2012-10-10 07:04:35 +0800

    cleanup

--- answer.d
+++ answer.d
@@ -469,7 +469,7 @@ void _unittest( string connParam )
     assert( r[0,10].as!PGbytea == [0x44, 0x20, 0x72, 0x75, 0x6c, 0x65, 0x73, 0x00, 0x21] ); // "D rules\x00!" (ASCII)
     
     immutable(int) i = 0;
-    auto v = r[0,11].asArray.getCell( 0, 1, 1 ).as!PGbytea.length;
+    auto v = r[0,11].asArray.getCell( 2, 1 ).as!PGbytea.length;
     //assert( v.size == 4 );
     
     writeln( "5: (unused) ", v );
