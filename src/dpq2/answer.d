7eded5118ff7341a8fa4a0dfed50dbf3428d3c97
Denis Feklushkin <denis.feklushkin@gmail.com>
2015-04-19 19:11:24 +0700

    added integration test what should fail

--- src/dpq2/answer.d
+++ src/dpq2/answer.d
@@ -582,6 +582,7 @@ void _integration_test( string connParam )
     assert( !a.isNULL(2,1,2) );
     
     assert( r[0].isNULL(12) );
+    assert( r[0][12].as!PGsmallint );
     assert( !r[0].isNULL(9) );
     assert( r[0][13].as!PGuuid.toString() == "8b9ab33a-96e9-499b-9c36-aad1fe86d640" );
         
