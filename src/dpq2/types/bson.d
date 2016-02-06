e054406cd1ee183fa0cb62f772e8dae26178d2cb
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-02-07 04:34:30 +0700

    Bson: PGreal support is removed due to https://github.com/rejectedsoftware/vibe.d/issues/1403

--- src/dpq2/types/bson.d
+++ src/dpq2/types/bson.d
@@ -103,11 +103,6 @@ private Bson rawValueToBson(const Value v)
             res = Bson(n);
             break;
 
-        case Float4:
-            auto n = to!double(v.as!PGreal);
-            res = Bson(n);
-            break;
-
         case Float8:
             double n = v.as!PGdouble_precision;
             res = Bson(n);
@@ -178,7 +173,6 @@ void _integration_test( string connParam )
         C(Bson(-32_761), "smallint", "-32761");
         C(Bson(-2_147_483_646), "integer", "-2147483646");
         C(Bson(-9_223_372_036_854_775_806), "bigint", "-9223372036854775806");
-        //C(Bson(-12.3456f), "real", "-12.3456"); // FIXME: https://github.com/rejectedsoftware/vibe.d/issues/1403
         C(Bson(-1234.56789012345), "double precision", "-1234.56789012345");
         C(Bson("first line\nsecond line"), "text", "'first line\nsecond line'");
 
