299adfeb84ad88268f1092ceda34e71863e43ee5
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-02-07 23:17:16 +0700

    formatting

--- src/dpq2/types/numeric.d
+++ src/dpq2/types/numeric.d
@@ -43,149 +43,149 @@ private // inner representation from libpq sources
         return str;
     }
 
-        /*
-         * get_str_from_var() -
-         *
-         *  Convert a var to text representation (guts of numeric_out).
-         *  The var is displayed to the number of digits indicated by its dscale.
-         *  Returns a palloc'd string.
-         */
-        string get_str_from_var(ref NumericVar var)
-        {
-            int         dscale;
-            char*       str;
-            char*       cp;
-            char*       endcp;
-            int         i;
-            int         d;
-            NumericDigit dig;
+    /*
+     * get_str_from_var() -
+     *
+     *  Convert a var to text representation (guts of numeric_out).
+     *  The var is displayed to the number of digits indicated by its dscale.
+     *  Returns a palloc'd string.
+     */
+    string get_str_from_var(ref NumericVar var)
+    {
+	int         dscale;
+	char*       str;
+	char*       cp;
+	char*       endcp;
+	int         i;
+	int         d;
+	NumericDigit dig;
 
-                static if(DEC_DIGITS > 1)
-                {
-                        NumericDigit d1;
-                }
+	    static if(DEC_DIGITS > 1)
+	    {
+		    NumericDigit d1;
+	    }
 
-            dscale = var.dscale;
+	dscale = var.dscale;
 
-            /*
-             * Allocate space for the result.
-             *
-             * i is set to the # of decimal digits before decimal point. dscale is the
-             * # of decimal digits we will print after decimal point. We may generate
-             * as many as DEC_DIGITS-1 excess digits at the end, and in addition we
-             * need room for sign, decimal point, null terminator.
-             */
-            i = (var.weight + 1) * DEC_DIGITS;
-            if (i <= 0)
-                i = 1;
+	/*
+	 * Allocate space for the result.
+	 *
+	 * i is set to the # of decimal digits before decimal point. dscale is the
+	 * # of decimal digits we will print after decimal point. We may generate
+	 * as many as DEC_DIGITS-1 excess digits at the end, and in addition we
+	 * need room for sign, decimal point, null terminator.
+	 */
+	i = (var.weight + 1) * DEC_DIGITS;
+	if (i <= 0)
+	    i = 1;
 
-            str = cast(char*)GC.malloc(i + dscale + DEC_DIGITS + 2);
-            cp = str;
+	str = cast(char*)GC.malloc(i + dscale + DEC_DIGITS + 2);
+	cp = str;
 
-            /*
-             * Output a dash for negative values
-             */
-            if (var.sign == NUMERIC_NEG)
-                *cp++ = '-';
+	/*
+	 * Output a dash for negative values
+	 */
+	if (var.sign == NUMERIC_NEG)
+	    *cp++ = '-';
 
-            /*
-             * Output all digits before the decimal point
-             */
-            if (var.weight < 0)
-            {
-                d = var.weight + 1;
-                *cp++ = '0';
-            }
-            else
-            {
-                for (d = 0; d <= var.weight; d++)
-                {
-                    dig = (d < var.digits.length) ? var.digits[d] : 0;
-                    /* In the first digit, suppress extra leading decimal zeroes */
-                    static if(DEC_DIGITS == 4)
-                    {
-                        bool putit = (d > 0);
+	/*
+	 * Output all digits before the decimal point
+	 */
+	if (var.weight < 0)
+	{
+	    d = var.weight + 1;
+	    *cp++ = '0';
+	}
+	else
+	{
+	    for (d = 0; d <= var.weight; d++)
+	    {
+		dig = (d < var.digits.length) ? var.digits[d] : 0;
+		/* In the first digit, suppress extra leading decimal zeroes */
+		static if(DEC_DIGITS == 4)
+		{
+		    bool putit = (d > 0);
 
-                        d1 = dig / 1000;
-                        dig -= d1 * 1000;
-                        putit |= (d1 > 0);
-                        if (putit)
-                            *cp++ = cast(char)(d1 + '0');
-                        d1 = dig / 100;
-                        dig -= d1 * 100;
-                        putit |= (d1 > 0);
-                        if (putit)
-                            *cp++ = cast(char)(d1 + '0');
-                        d1 = dig / 10;
-                        dig -= d1 * 10;
-                        putit |= (d1 > 0);
-                        if (putit)
-                            *cp++ = cast(char)(d1 + '0');
-                        *cp++ = cast(char)(dig + '0');
-                    }
-                    else static if(DEC_DIGITS == 2)
-                    {
-                            d1 = dig / 10;
-                            dig -= d1 * 10;
-                            if (d1 > 0 || d > 0)
-                                *cp++ = cast(char)(d1 + '0');
-                            *cp++ = cast(char)(dig + '0');
-                    }
-                    else static if(DEC_DIGITS == 1)
-                    {
-                        *cp++ = cast(char)(dig + '0');
-                    }
-                    else pragma(error, "unsupported NBASE");
-                }
-            }
+		    d1 = dig / 1000;
+		    dig -= d1 * 1000;
+		    putit |= (d1 > 0);
+		    if (putit)
+			*cp++ = cast(char)(d1 + '0');
+		    d1 = dig / 100;
+		    dig -= d1 * 100;
+		    putit |= (d1 > 0);
+		    if (putit)
+			*cp++ = cast(char)(d1 + '0');
+		    d1 = dig / 10;
+		    dig -= d1 * 10;
+		    putit |= (d1 > 0);
+		    if (putit)
+			*cp++ = cast(char)(d1 + '0');
+		    *cp++ = cast(char)(dig + '0');
+		}
+		else static if(DEC_DIGITS == 2)
+		{
+			d1 = dig / 10;
+			dig -= d1 * 10;
+			if (d1 > 0 || d > 0)
+			    *cp++ = cast(char)(d1 + '0');
+			*cp++ = cast(char)(dig + '0');
+		}
+		else static if(DEC_DIGITS == 1)
+		{
+		    *cp++ = cast(char)(dig + '0');
+		}
+		else pragma(error, "unsupported NBASE");
+	    }
+	}
 
-            /*
-             * If requested, output a decimal point and all the digits that follow it.
-             * We initially put out a multiple of DEC_DIGITS digits, then truncate if
-             * needed.
-             */
-            if (dscale > 0)
-            {
-                *cp++ = '.';
-                endcp = cp + dscale;
-                for (i = 0; i < dscale; d++, i += DEC_DIGITS)
-                {
-                    dig = (d >= 0 && d < var.digits.length) ? var.digits[d] : 0;
-                    static if(DEC_DIGITS == 4)
-                    {
-                            d1 = dig / 1000;
-                            dig -= d1 * 1000;
-                            *cp++ = cast(char)(d1 + '0');
-                            d1 = dig / 100;
-                            dig -= d1 * 100;
-                            *cp++ = cast(char)(d1 + '0');
-                            d1 = dig / 10;
-                            dig -= d1 * 10;
-                            *cp++ = cast(char)(d1 + '0');
-                            *cp++ = cast(char)(dig + '0');
-                    }
-                    else static if(DEC_DIGITS == 2)
-                    {
-                            d1 = dig / 10;
-                            dig -= d1 * 10;
-                            *cp++ = cast(char)(d1 + '0');
-                            *cp++ = cast(char)(dig + '0');
-                    }
-                    else static if(DEC_DIGITS == 1)
-                    {
-                        *cp++ = cast(char)(dig + '0');
-                }
-                else pragma(error, "unsupported NBASE");
-                }
-                cp = endcp;
-            }
+	/*
+	 * If requested, output a decimal point and all the digits that follow it.
+	 * We initially put out a multiple of DEC_DIGITS digits, then truncate if
+	 * needed.
+	 */
+	if (dscale > 0)
+	{
+	    *cp++ = '.';
+	    endcp = cp + dscale;
+	    for (i = 0; i < dscale; d++, i += DEC_DIGITS)
+	    {
+		dig = (d >= 0 && d < var.digits.length) ? var.digits[d] : 0;
+		static if(DEC_DIGITS == 4)
+		{
+			d1 = dig / 1000;
+			dig -= d1 * 1000;
+			*cp++ = cast(char)(d1 + '0');
+			d1 = dig / 100;
+			dig -= d1 * 100;
+			*cp++ = cast(char)(d1 + '0');
+			d1 = dig / 10;
+			dig -= d1 * 10;
+			*cp++ = cast(char)(d1 + '0');
+			*cp++ = cast(char)(dig + '0');
+		}
+		else static if(DEC_DIGITS == 2)
+		{
+			d1 = dig / 10;
+			dig -= d1 * 10;
+			*cp++ = cast(char)(d1 + '0');
+			*cp++ = cast(char)(dig + '0');
+		}
+		else static if(DEC_DIGITS == 1)
+		{
+		    *cp++ = cast(char)(dig + '0');
+	    }
+	    else pragma(error, "unsupported NBASE");
+	    }
+	    cp = endcp;
+	}
 
-            /*
-             * terminate the string and return it
-             */
-            *cp = '\0';
-            return str.fromStringz.idup;
-        }
+	/*
+	 * terminate the string and return it
+	 */
+	*cp = '\0';
+	return str.fromStringz.idup;
+    }
 }
 
 struct PGNumeric
