f8afef87b4f70b1eb7aa30e5581ec1dab4290ec4
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-02-23 22:31:05 +0700

    dscanner start fix

--- .test.sh
+++ .test.sh
@@ -5,7 +5,7 @@ dub test
 
 dub run dpq2:integration_tests --build=unittest-cov -- --conninfo="${1}"
 
-if [[ ${DC} -eq "dmd" ]]; then dub run dscanner -- -s; fi
-#if [[ ${DC} -eq "dmd" ]]; then dub run dscanner -- -S; fi #disabled due to assertion failure in dsymbol
+if [ "$DC" == "dmd" ]; then dub run dscanner -- -s; fi
+#if [ "$DC" == "dmd" ]; then dub run dscanner -- -S; fi #disabled due to assertion failure in dsymbol
 
 dub run dpq2:example --build=release -- --conninfo="${1}"
