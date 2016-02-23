9cdaf5fd52f1b5739eea79710060625c2c81f1c1
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-02-23 23:03:19 +0700

    example only for dmd

--- .test.sh
+++ .test.sh
@@ -8,4 +8,4 @@ dub run dpq2:integration_tests --build=unittest-cov -- --conninfo="${1}"
 if [ "$DC" == "dmd" ]; then dub run dscanner -- -s; fi
 #if [ "$DC" == "dmd" ]; then dub run dscanner -- -S; fi #disabled due to assertion failure in dsymbol
 
-dub run dpq2:example --build=release -- --conninfo="${1}"
+if [ "$DC" == "dmd" ]; then dub run dpq2:example --build=release -- --conninfo="${1}"; fi
