c22baa78e983078c5d156c630069ccf1e78649da
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-02-23 23:10:59 +0700

    gdc temp disable

--- src/dpq2/package.d
+++ src/dpq2/package.d
@@ -1,15 +1,37 @@
 ﻿module dpq2;
 
+debug import std.experimental.logger;
+
 shared static this()
 {
+    debug
+    {
+        trace("DerelictPQ loading...");
+    }
+
     DerelictPQ.load();
+
+    debug
+    {
+        trace("...DerelictPQ loading finished");
+    }
 }
 
 shared static ~this()
 {
+    debug
+    {
+        trace("DerelictPQ unloading...");
+    }
+
     import core.memory: GC;
     GC.collect();
     DerelictPQ.unload();
+
+    debug
+    {
+        trace("DerelictPQ unloading finished");
+    }
 }
 
 /// Base for all dpq2 exceptions classes
