7a51153ac54237db044593af8303d2572e619398
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-03-13 08:19:51 +0700

    from_d_types.d added

---
+++ src/dpq2/types/from_d_types.d
@@ -0,0 +1,45 @@
+module dpq2.types.from_d_types;
+
+@trusted:
+package:
+
+import dpq2;
+import std.bitmanip: nativeToBigEndian;
+import std.traits: isNumeric;
+
+Value toValue(T)(T v)
+if(isNumeric!(T))
+{
+    return Value(v.nativeToBigEndian.dup, v.detectOidType, false, ValueFormat.BINARY);
+}
+
+unittest
+{
+    {
+        Value v = toValue(cast(short) 123);
+
+        assert(v.oidType == OidType.Int2);
+        assert(v.as!short == 123);
+    }
+
+    {
+        Value v = toValue(-123.456);
+
+        assert(v.oidType == OidType.Float8);
+        assert(v.as!double == -123.456);
+    }
+}
+
+private OidType detectOidType(T)(T v)
+{
+    with(OidType)
+    {
+        static if(is(T == short)){ return Int2; } else
+        static if(is(T == int)){ return Int4; } else
+        static if(is(T == long)){ return Int8; } else
+        static if(is(T == float)){ return Float4; } else
+        static if(is(T == double)){ return Float8; } else
+
+        static assert(false, "Unsupported D type: "~T.stringof);
+    }
+}
