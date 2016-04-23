9608bea0c18309a15c578db413f1e046b4402035
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-04-23 13:27:23 +0700

    PQgetvalue call clearing

--- src/dpq2/result.d
+++ src/dpq2/result.d
@@ -322,8 +322,8 @@ immutable struct Row
         // The pointer returned by PQgetvalue points to storage that is part of the PGresult structure.
         // One should not modify the data it points to, and one must explicitly copy the data into other
         // storage if it is to be used past the lifetime of the PGresult structure itself.
-        auto v = cast(immutable) PQgetvalue(answer.result, to!int(row), to!int(col));
-        auto s = size( col );
+        const ubyte* v = cast(immutable) PQgetvalue(answer.result, to!int(row), to!int(col));
+        size_t s = size(col);
 
         // it is legal to cast here because immutable value will be returned
         Value r = Value(cast(ubyte[]) v[0..s], answer.OID(col), isNULL(col), answer.columnFormat(col));
