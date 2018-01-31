92d8cbad69a2e00080550e343be58c5660c6237e
Unknown <chalucha@gmail.com>
2018-01-31 19:35:12 +0100

    Avoid adjusting TZ set in PostgreSQL for integration tests

--- src/dpq2/conv/to_d_types.d
+++ src/dpq2/conv/to_d_types.d
@@ -177,8 +177,12 @@ if( is( T == Json ) )
 
 public void _integration_test( string connParam ) @system
 {
+    import std.algorithm : endsWith;
+    import std.array : replace;
+    import std.format : format;
+    import std.math : abs;
+
     auto conn = new Connection(connParam);
-    conn.exec("set timezone to 0"); // to return times in UTC and so make the test reproducible in databases with other TZ
 
     QueryParams params;
     params.resultFormat = ValueFormat.BINARY;
@@ -275,7 +279,19 @@ public void _integration_test( string connParam ) @system
         C!PGtimestamp_without_time_zone(TimeStampWithoutTZ(DateTime(1997, 12, 17, 7, 37, 16), dur!"usecs"(12)), "timestamp without time zone", "'1997-12-17 07:37:16.000012'");
         C!PGtimestamp_without_time_zone(TimeStampWithoutTZ.max, "timestamp without time zone", "'infinity'");
         C!PGtimestamp_without_time_zone(TimeStampWithoutTZ.min, "timestamp without time zone", "'-infinity'");
-        C!SysTime(SysTime(DateTime(1997, 12, 17, 7, 37, 16), dur!"usecs"(12), new immutable SimpleTimeZone(2.dur!"hours")), "timestamp with time zone", "'1997-12-17 05:37:16.000012+00'");
+
+        // systime testing
+        auto testTZ = new immutable SimpleTimeZone(2.dur!"hours"); // custom TZ
+        auto sysTime = SysTime(DateTime(1997, 12, 17, 7, 37, 16), dur!"usecs"(12), testTZ); // time in custom TZ
+        auto sysTimeText = sysTime.toLocalTime.toISOExtString.replace("T", " "); // expected text to check against
+
+        // append LocalTime offset (SysTime.toISOExtString in LocalTime doesn't append the TZ offset)
+        auto splitRes = LocalTime().utcOffsetAt(sysTime.stdTime).split!("hours", "minutes");
+        sysTimeText ~= splitRes.hours > 0 ? "+" : "-";
+        sysTimeText ~= format!"%02d"(abs(splitRes.hours));
+        if (splitRes.minutes > 0) sysTimeText ~= format!":%02d"(abs(splitRes.minutes));
+
+        C!SysTime(sysTime, "timestamp with time zone", "'"~sysTimeText~"'");
 
         // json
         C!PGjson(Json(["float_value": Json(123.456), "text_str": Json("text string")]), "json", `'{"float_value": 123.456,"text_str": "text string"}'`);
