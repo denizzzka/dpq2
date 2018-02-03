113c7c907aaff58c2a1930577ef1e24245cf81df
Denis Feklushkin <denis.feklushkin@gmail.com>
2018-02-04 02:23:45 +0700

    comment about jdate

--- src/dpq2/conv/time.d
+++ src/dpq2/conv/time.d
@@ -183,7 +183,7 @@ void j2date(int jd, out int year, out int month, out int day)
 {
     enum MONTHS_PER_YEAR = 12;
     enum POSTGRES_EPOCH_JDATE = POSTGRES_EPOCH_DATE.julianDay;
-    static assert(POSTGRES_EPOCH_JDATE == 2_451_545);
+    static assert(POSTGRES_EPOCH_JDATE == 2_451_545); // value from Postgres code
 
     jd += POSTGRES_EPOCH_JDATE;
 
