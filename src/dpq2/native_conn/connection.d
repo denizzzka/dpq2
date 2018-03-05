28afb43efc4d3dd7d915d8424bef4161d8678fad
Denis Feklushkin <denis.feklushkin@gmail.com>
2018-03-05 14:33:05 +0700

    dub.json changes, waitEndOfRead added (not works)

--- src/dpq2/native_conn/connection.d
+++ src/dpq2/native_conn/connection.d
@@ -1,6 +1,8 @@
 module dpq2.native_conn.connection;
 
 import std.socket;
+import std.datetime: Duration;
+import dpq2.connection: ConnectionException;
 
 class NativeConnection
 {
@@ -14,10 +16,36 @@ class NativeConnection
         auto addr = new InternetAddress(host, port);
         socket.connect(addr);
     }
+
+    void waitEndOfRead(in Duration timeout)
+    {
+        //TODO: make waiting independent from available event library
+        import vibe.core.core;
+
+        version(Posix)
+        {
+            import core.sys.posix.fcntl;
+            assert((fcntl(socket.handle, F_GETFL, 0) & O_NONBLOCK), "Socket assumed to be non-blocking already");
+        }
+
+        auto event = createFileDescriptorEvent(socket.handle, FileDescriptorEvent.Trigger.read);
+
+        do
+        {
+            if(!event.wait(timeout))
+                throw new ConnectionException("Connection error", __FILE__, __LINE__);
+
+            //~ consumeInput();
+        }
+        while(true); // (this.isBusy); // wait until PQgetresult won't block anymore
+    }
 }
 
 version (integration_tests)
 void _integration_test(string host, ushort port)
 {
+    import std.datetime;
+
     auto conn = new NativeConnection(host, port);
+    //~ conn.waitEndOfRead(1.seconds);
 }
