33b5b0cd28b254f024ff6e9bda7c6d164ddc312d
Denis Feklushkin <feklushkin.denis@gmail.com>
2021-12-16 14:28:38 +0300

    class DynamicLoader added

--- src/dpq2/package.d
+++ src/dpq2/package.d
@@ -5,44 +5,78 @@
  */
 module dpq2;
 
-import derelict.pq.pq;
-debug import std.experimental.logger;
+public import dpq2.connection;
+public import dpq2.query;
+public import dpq2.result;
+public import dpq2.oids;
 
+//FIXME: delete this
 version(DerelictPQ_Static){}
 else
 {
-    static __gshared bool __initialized;
+DynamicLoader dl;
+
+static this()
+{
+    dl = new DynamicLoader;
+}
+
+static ~this()
+{
+    dl.destroy;
+}
+
+}
+
+version(DerelictPQ_Static){}
+else
+class DynamicLoader
+{
+    import derelict.pq.pq: DerelictPQ;
+    import core.sync.mutex: Mutex;
+    import core.atomic;
+    debug import std.experimental.logger;
+
+    private __gshared static Mutex mutex;
+    private shared static ptrdiff_t instances;
+
+    shared static this()
+    {
+        mutex = new Mutex();
+    }
 
     /*
-        Not shared static constructor makes possible to setup
-        DerelictPQ.missingSymbolCallback from external static
-        constructors. So it is possible to use this library even with
+        It is possible to setup DerelictPQ.missingSymbolCallback
+        before. So it is possible to use this library even with
         previous versions of libpq with some missing symbols.
     */
-    static this()
+    this()
     {
-        import std.concurrency : initOnce;
-        initOnce!__initialized({
-            debug
-            {
-                trace("DerelictPQ loading...");
-            }
+        mutex.lock();
+        scope(exit) mutex.unlock();
+
+        if(instances.atomicFetchAdd(1) == 0)
+        {
+            debug trace("DerelictPQ loading...");
 
             DerelictPQ.load();
 
-            debug
-            {
-                trace("...DerelictPQ loading finished");
-            }
-            return true;
-        }());
+            debug trace("...DerelictPQ loading finished");
+        }
     }
-}
 
-public
-{
-    import dpq2.connection;
-    import dpq2.query;
-    import dpq2.result;
-    import dpq2.oids;
+    ~this()
+    {
+        mutex.lock();
+        scope(exit) mutex.unlock();
+
+        if(instances.atomicFetchSub(1) == 1)
+        {
+            debug trace("DerelictPQ unloading...");
+
+            DerelictPQ.unload();
+
+            debug trace("...DerelictPQ unloading finished");
+        }
+    }
 }
